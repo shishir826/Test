fbf015b9c28e7a4494bfee152185b36fb228698e
shishir826 <ghimireshishir828@gmail.com>
2026-01-09 22:50:01 +0545

    neww

--- script.js
+++ script.js
@@ -15,22 +15,26 @@ function go() {
         return;
     }
 
-    if(name === "shishir") {
+    else if(name === "shishir") {
         a.innerHTML = "How can you even think of it????? He is straight as fuck!!";
         return;
     }
 
-    if(y.includes(name)) {
+    else if(y.includes(name)) {
         a.innerHTML = "You are Gay, No Doubt!";
         return;
     }
 
-    if(z.includes(name)) {
+   else if(z.includes(name)) {
         a.innerHTML = "Nah she is Angel..";
         return;
     }
 
     // If name not found in either array
+    else
+   {
     a.innerHTML = "So you doubt yourself if you are gay or not, that's the reason why you tested right? Hell yeah, you're gay then....";
+    }
 }
 </script>
+
