77611dde5bc1e4b53935787588a31b6fd9a481b9
shishir826 <ghimireshishir828@gmail.com>
2026-01-09 22:45:16 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
+<input type="text" id="name" placeholder="Enter name">
+<button id="submit" onclick="go()">Check</button>
+<p id="res"></p>
 
-let x = document.getElementById("submit");
+<script>
 let a = document.getElementById("res");
 let z = ["sejal","aayushma","supriya","yashoda","aaha"];
 let y = ["safal","paras","sumit","nisip","pratyush","titan","suyog","abhinav"];
-function go(){
-    let name = document.getElementById("name").value;
-    name = toLowerCase(name);
-    for(let i = 0;i<y.length;i++)
-    {
-        if(name == y[i])
-        {
-            a.innerHTML = "You are Gay, No Doubt!";
-            break;
-        }
-        else if(name == "shishir")
-        {
-            a.innerHTML = "How can you even think of it????? He is straight as fuck!!"
-            break;
-        }
-        else if(name == z[i] )
-        {
-            a.innerHTML = "Nah she is Angel..";
-            break;
-        }
-        else if(name.length < 3)
-        {
-            a.innerHTML = "Please use valid name.";
-            break;
-        }
-        else 
-        {
-            a.innerHTML = "So you doubt yourself if you are gay or not,that's the reason why you tested right? Hell yeah you're gay then....";
-            continue;
-        }
+
+function go() {
+    let name = document.getElementById("name").value.toLowerCase();
+
+    if(name.length < 3) {
+        a.innerHTML = "Please use a valid name.";
+        return;
     }
+
+    if(name === "shishir") {
+        a.innerHTML = "How can you even think of it????? He is straight as fuck!!";
+        return;
+    }
+
+    if(y.includes(name)) {
+        a.innerHTML = "You are Gay, No Doubt!";
+        return;
+    }
+
+    if(z.includes(name)) {
+        a.innerHTML = "Nah she is Angel..";
+        return;
+    }
+
+    // If name not found in either array
+    a.innerHTML = "So you doubt yourself if you are gay or not, that's the reason why you tested right? Hell yeah, you're gay then....";
 }
-
-
+</script>
