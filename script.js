22af6a1cab89f74f09c4148c0ba2521306b06c59
shishir826 <ghimireshishir828@gmail.com>
2026-01-10 12:24:45 +0545

    copy pasate

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-
+let x = document.getElementById("submit");
 let a = document.getElementById("res");
 let z = ["sejal","aayushma","supriya","yashoda","aaha"];
 let y = ["safal","paras","sumit","nisip","pratyush","titan","suyog","abhinav"];
-
-function go() {
+function go(){
     let name = document.getElementById("name").value.toLowerCase();
-
-    if(name.length < 3) {
-        a.innerHTML = "Please use a valid name.";
-        return;
-    }
-
-    else if(name == "shishir") {
-        a.innerHTML = "How can you even think of it????? He is straight as fuck!!";
-        return;
-    }
-
-    else if(y.includes(name)) {
-        a.innerHTML = "You are Gay, No Doubt!";
-        return;
-    }
-
-   else if(z.includes(name)) {
-        a.innerHTML = "Nah she is Angel..";
-        return;
-    }
-
-    // If name not found in either array
-    else
-   {
-    a.innerHTML = "So you doubt yourself if you are gay or not, that's the reason why you tested right? Hell yeah, you're gay then....";
+    for(let i = 0;i<y.length;i++)
+    {
+        if(name == y[i])
+        {
+            a.innerHTML = "You are Gay, No Doubt!";
+            break;
+        }
+        else if(name == "shishir")
+        {
+            a.innerHTML = "How can you even think of it????? He is straight as fuck!!"
+            break;
+        }
+        else if(name == z[i] )
+        {
+            a.innerHTML = "Nah she is Angel..";
+            break;
+        }
+        else if(name.length < 3)
+        {
+            a.innerHTML = "Please use valid name.";
+            break;
+        }
+        else 
+        {
+            a.innerHTML = "So you doubt yourself if you are gay or not,that's the reason why you tested right? Hell yeah you're gay then....";
+            continue;
+        }
     }
 }
-</script>
-
-
